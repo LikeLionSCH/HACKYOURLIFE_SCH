707d91c9da31103f7394a2b0c3bda43ab5c8a14b
KimTaewan <wwan13@naver.com>
2020-11-20 00:05:27 +0900

    [taewan] base 스타일 수정

---
+++ base/base.js
@@ -0,0 +1,8 @@
+// $(document).ready(function () {
+//     $(".__menu>li").mouseover(function () {
+//         $(this).children(".__depth2").stop().slideDown(100);
+//     });
+//     $(".__menu>li").mouseleave(function () {
+//         $(this).children(".__depth2").stop().slideUp(100);
+//     });
+// });
