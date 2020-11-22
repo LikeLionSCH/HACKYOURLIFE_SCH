67adc03a007fbfa0f0c8f6003670012c44b382f2
KimTaewan <wwan13@naver.com>
2020-11-23 01:50:24 +0900

    [taewan] : 쓸모없는 파일 삭제

--- base/base.js
+++
@@ -1,8 +0,0 @@
-// $(document).ready(function () {
-//     $(".__menu>li").mouseover(function () {
-//         $(this).children(".__depth2").stop().slideDown(100);
-//     });
-//     $(".__menu>li").mouseleave(function () {
-//         $(this).children(".__depth2").stop().slideUp(100);
-//     });
-// });
