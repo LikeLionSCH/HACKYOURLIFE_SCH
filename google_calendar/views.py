722fc02680f04de59d4613b9cd789c8438460801
HwangSB <hsb990105@gmail.com>
2020-11-30 01:51:02 +0900

    [HwangSB]: 캘린더에서 앞으로 예정된 10개의 일정 조회 가능

--- google_calendar/views.py
+++ google_calendar/views.py
@@ -1,48 +1,34 @@
 # django modules
 from django.shortcuts import render
+from django.http import HttpResponse
 
 import datetime
-import pickle
 
 # google auth modules
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 
+# shared calendar id
+CALENDAR_ID = 'schlikelion2020@gmail.com'
+# google calendar api key
+API_KEY = 'AIzaSyCTqmmO5-26iFUs0RWgGvaKjMSpvXqrv08'
 # google calendar permission: read only
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 # calendar view
 def calendar(request):
-    credentials = None
-    
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            credentials = pickle.load(token)
-
-    if not credentials or not credentials.valid:
-        if credentials and credentials.expired and credentials.refresh_token:
-            credentials.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            credentials = flow.run_local_server(port=0)
-
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(credentials, token)
-
-    service = build('calendar', 'v3', credentials=credentials)
+    service = build('calendar', 'v3', developerKey=API_KEY)
 
     now = datetime.datetime.utcnow().isoformat() + 'Z'
-    events_result = service.events().list(calendarId='primary', timeMin=now, maxResults=10, singleEvents=True, orderBy='startTime').execute()
+    events_result = service.events().list(calendarId=CALENDAR_ID, timeMin=now, maxResults=10, singleEvents=True, orderBy='startTime').execute()
     events = events_result.get('items', [])
 
     if not events:
         return render(request, 'calendar.html')
 
-    start_times = []
-    summaries = []
+    data = []
     for event in events:
-        start_times.append(event['start'].get('dateTime', event['start'].get('date')))
-        summaries.append(event['summary'])
+        start_time = event['start'].get('dateTime', event['start'].get('date'))
+        summary = event['summary']
+        data.append((start_time, summary))
 
-    return render(request, 'calendar.html', {'start_times': start_times, 'summaries': summaries})
+    return render(request, 'calendar.html', {'data': data})
