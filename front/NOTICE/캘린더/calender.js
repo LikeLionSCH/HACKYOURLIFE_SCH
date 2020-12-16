5b45428ec6c211334b05ed2c7a2a7a7f1638132c
minsgy <alstjrchl97@naver.com>
2020-12-16 20:31:34 +0900

    [minseok, white] : calender page  create

---
+++ front/NOTICE/캘린더/calender.js
@@ -0,0 +1,72 @@
+$(function() {
+
+    $('#calendar').fullCalendar({
+      defaultView: 'month',
+      defaultDate: '2019-01-12',
+  
+      eventRender: function(eventObj, $el) {
+        $el.popover({
+          title: eventObj.title,
+          content: eventObj.description,
+          trigger: 'hover',
+          placement: 'top',
+          container: 'body'
+        });
+      },
+  
+      events: [
+        
+        {
+          title: 'Long Event',
+          description: 'description for Long Event',
+          start: '2019-01-07',
+          end: '2019-01-10'
+        },
+        {
+          id: 999,
+          title: 'Repeating Event',
+          description: 'description for Repeating Event',
+          start: '2019-01-09T16:00:00'
+        },
+        {
+          id: 999,
+          title: 'Repeating Event',
+          description: 'description for Repeating Event',
+          start: '2019-01-16T16:00:00'
+        },
+        {
+          title: 'Conference',
+          description: 'description for Conference',
+          start: '2019-01-11',
+          end: '2019-01-13'
+        },
+        {
+          title: 'Meeting',
+          description: 'description for Meeting',
+          start: '2019-01-12'
+        },
+        {
+          title: 'Lunch',
+          description: 'description for Lunch',
+          start: '2019-01-13'
+        },
+        {
+          title: 'ㅎㅇㄹ',
+          description: 'description for Meeting',
+          start: '2019-01-12'
+        },
+        {
+          title: 'Birthday Party',
+          description: 'description for Birthday Party',
+          start: '2019-01-13T07:00:00'
+        },
+        {
+          title: '기말 고사',
+          description: 'description for Birthday Party',
+          start: '2020-12-07',
+          end: '2020-12-15'
+        },
+      ]
+    });
+  
+  });
