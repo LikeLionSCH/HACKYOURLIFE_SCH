2dd91a394a5e746c0b65f7b1a378bcc317dd7fa2
김율희 <dbfgml741@likelion.org>
2020-12-19 22:02:48 +0900

    [yulhee] notice CRUD

--- notice/urls.py
+++ notice/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     #path('notice/detail', views.notice_detail, name="notice_detail"), # 페이지 preview용
     path('faq/', views.faq, name="faq"),
     path('notice/create', views.notice_create, name="notice_create"),
+    path('notice/delete/<str:notice_id>/',views.notice_delete,name='notice_delete'),
+    path('notice/update/<str:notice_id>/',views.notice_update,name='notice_update'),
 ]
