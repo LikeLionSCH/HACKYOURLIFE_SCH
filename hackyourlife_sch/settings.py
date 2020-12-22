cc8185497ef715de317d9408c88819b262e68889
ningpop <craft1933@naver.com>
2020-12-22 16:46:36 +0900

    [ningpop] : add main app and connect main page to django with static files

--- hackyourlife_sch/settings.py
+++ hackyourlife_sch/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    Path(BASE_DIR, 'static')
+]
+
+# STATIC_ROOT = Path(BASE_DIR, 'static')
