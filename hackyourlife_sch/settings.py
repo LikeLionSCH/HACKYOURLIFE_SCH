09408a79e9386ebe8680a6edd5edbcf5d536d1d3
ningpop <craft1933@gmail.com>
2020-12-23 23:04:01 +0900

    [ningpop] : add about app config

--- hackyourlife_sch/settings.py
+++ hackyourlife_sch/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
+    'about.apps.AboutConfig',
     'assignments.apps.AssignmentsConfig',
     'session.apps.SessionConfig',
 ]
