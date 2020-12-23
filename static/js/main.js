b9dbf20db2f7c79c6e33664d82b85df65a7e61e2
KimTaewan <wwan13@naver.com>
2020-12-24 00:16:58 +0900

    [taewan] : static 파일 재정렬

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 /* main.html deadline에 [yyyy/mm/dd 00:00:00] 형식으로 입력 시 자동 적용*/
 var choice_datetime = $('#deadline-time').text();
-var targetDate = new Date(choice_datetime);   
+var targetDate = new Date(choice_datetime);
 var days;
 var hrs;
 var min;
@@ -9,49 +9,49 @@ var sec;
 
 /*----------------- CURRICULUM hover 효과 삽입----------- */
 // 1
-function firstTextChange(obj){
-    obj.innerHTML="3월-4월";
-    obj.style.color="white";
-    obj.style.fontfamily='IBMPlexSansKR-Text';
-   }
+function firstTextChange(obj) {
+  obj.innerHTML = "3월-4월";
+  obj.style.color = "white";
+  obj.style.fontfamily = 'IBMPlexSansKR-Text';
+}
 
-   function firstTextChangeAgain(obj){
-    obj.innerHTML="UI/UX 설계";
-   }
+function firstTextChangeAgain(obj) {
+  obj.innerHTML = "UI/UX 설계";
+}
 // 2
-function secondTextChange(obj){
-    obj.innerHTML="5월-6월";
-    obj.style.color="white";
-    obj.style.fontfamily='IBMPlexSansKR-Text';
-   }
+function secondTextChange(obj) {
+  obj.innerHTML = "5월-6월";
+  obj.style.color = "white";
+  obj.style.fontfamily = 'IBMPlexSansKR-Text';
+}
 
-function secondTextChangeAgain(obj){
-    obj.innerHTML="HTML/CSS";
-   } 
+function secondTextChangeAgain(obj) {
+  obj.innerHTML = "HTML/CSS";
+}
 
 // 3
 
-function ThirdTextChange(obj){
-    obj.innerHTML="7월-10월";
-    obj.style.color="white";
-    obj.style.fontfamily='IBMPlexSansKR-Text';
-   }
+function ThirdTextChange(obj) {
+  obj.innerHTML = "7월-10월";
+  obj.style.color = "white";
+  obj.style.fontfamily = 'IBMPlexSansKR-Text';
+}
 
-function ThirdTextChangeAgain(obj){
-    obj.innerHTML="Python/Django";
-   } 
+function ThirdTextChangeAgain(obj) {
+  obj.innerHTML = "Python/Django";
+}
 
 //  4
 
-function fourthTextChange(obj){
-    obj.innerHTML="11월-12월";
-    obj.style.color="white";
-    obj.style.fontfamily='IBMPlexSansKR-Text';
-   }
+function fourthTextChange(obj) {
+  obj.innerHTML = "11월-12월";
+  obj.style.color = "white";
+  obj.style.fontfamily = 'IBMPlexSansKR-Text';
+}
 
-function fourthTextChangeAgain(obj){
-    obj.innerHTML="해커톤";
-   } 
+function fourthTextChangeAgain(obj) {
+  obj.innerHTML = "해커톤";
+}
 
 
 /*--------------CountDown 배너 ------------- */
@@ -65,77 +65,77 @@ function fourthTextChangeAgain(obj){
 /* --------------------------
  * 로드 값
  * -------------------------- */
-$(function() {
-   // Calculate time until launch date
-   timeToLaunch();
+$(function () {
+  // Calculate time until launch date
+  timeToLaunch();
   // Transition the current countdown from 0 
   numberTransition('#days .number', days, 1000, 'easeOutQuad');
   numberTransition('#hours .number', hrs, 1000, 'easeOutQuad');
   numberTransition('#minutes .number', min, 1000, 'easeOutQuad');
   numberTransition('#seconds .number', sec, 1000, 'easeOutQuad');
   // Begin Countdown
-  setTimeout(countDownTimer,1001);
+  setTimeout(countDownTimer, 1001);
 });
 
 /* --------------------------
  * FIGURE OUT THE AMOUNT OF 
    TIME LEFT BEFORE LAUNCH
  * -------------------------- */
-function timeToLaunch(){
-    // Get the current date
-    var currentDate = new Date();
+function timeToLaunch() {
+  // Get the current date
+  var currentDate = new Date();
 
-    // Find the difference between dates
-    var diff = (currentDate - targetDate)/1000;
-    var diff = Math.abs(Math.floor(diff));  
+  // Find the difference between dates
+  var diff = (currentDate - targetDate) / 1000;
+  var diff = Math.abs(Math.floor(diff));
 
-    // Check number of days until target
-    days = Math.floor(diff/(24*60*60));
-    sec = diff - days * 24*60*60;
+  // Check number of days until target
+  days = Math.floor(diff / (24 * 60 * 60));
+  sec = diff - days * 24 * 60 * 60;
 
-    // Check number of hours until target
-    hrs = Math.floor(sec/(60*60));
-    sec = sec - hrs * 60*60;
+  // Check number of hours until target
+  hrs = Math.floor(sec / (60 * 60));
+  sec = sec - hrs * 60 * 60;
 
-    // Check number of minutes until target
-    min = Math.floor(sec/(60));
-    sec = sec - min * 60;
+  // Check number of minutes until target
+  min = Math.floor(sec / (60));
+  sec = sec - min * 60;
 }
 
 /* --------------------------
  * 화면에 보여줄 값
  * -------------------------- */
-function countDownTimer(){ 
-    
-    // Figure out the time to launch
-    timeToLaunch();
-    
-    // Write to countdown component
-    $( "#days .number" ).text(days);
-    $( "#hours .number" ).text(hrs);
-    $( "#minutes .number" ).text(min);
-    $( "#seconds .number" ).text(sec);
-    
-    // Repeat the check every second
-    setTimeout(countDownTimer,1000);
+function countDownTimer() {
+
+  // Figure out the time to launch
+  timeToLaunch();
+
+  // Write to countdown component
+  $("#days .number").text(days);
+  $("#hours .number").text(hrs);
+  $("#minutes .number").text(min);
+  $("#seconds .number").text(sec);
+
+  // Repeat the check every second
+  setTimeout(countDownTimer, 1000);
 }
 
 /* --------------------------
  * TRANSITION NUMBERS FROM 0
    TO CURRENT TIME UNTIL LAUNCH
  * -------------------------- */
-function numberTransition(id, endPoint, transitionDuration, transitionEase){
+function numberTransition(id, endPoint, transitionDuration, transitionEase) {
   // Transition numbers from 0 to the final number
-  $({numberCount: $(id).text()}).animate({numberCount: endPoint}, {
-      duration: transitionDuration,
-      easing:transitionEase,
-      step: function() {
-        $(id).text(Math.floor(this.numberCount));
-      },
-      complete: function() {
-        $(id).text(this.numberCount);
-      }
-   }); 
+  $({ numberCount: $(id).text() }).animate({ numberCount: endPoint }, {
+    duration: transitionDuration,
+    easing: transitionEase,
+    step: function () {
+      $(id).text(Math.floor(this.numberCount));
+    },
+    complete: function () {
+      $(id).text(this.numberCount);
+    }
+  });
 };
 
 function getCookie(name) {
@@ -145,15 +145,15 @@ function getCookie(name) {
     for (let i = 0; i < cookies.length; i++) {
       let cookie = cookies[i].trim();
       if (cookie.substring(0, name.length + 1) === (name + '=')) {
-          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-          break;
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
       }
     }
   }
   return cookieValue;
 }
 
-async function googleSignIn(){
+async function googleSignIn() {
   const firebaseConfig = {
     apiKey: "",
     authDomain: "likelion-sch.firebaseapp.com",
@@ -168,17 +168,17 @@ async function googleSignIn(){
   firebase.initializeApp(firebaseConfig);
 
   let provider = new firebase.auth.GoogleAuthProvider();
-  await firebase.auth().signInWithPopup(provider).then(function(result) {
+  await firebase.auth().signInWithPopup(provider).then(function (result) {
     // let accessToken = result.credential.accessToken;
     // let idToken = result.credential.idToken;
     let uid = result.user.uid;
     $.ajax({
       type: "POST",
-      url:"/",
+      url: "/",
       headers: {
         "X-CSRFToken": getCookie('csrftoken')
       },
-      data: {uid: uid},
+      data: { uid: uid },
       dataType: "json"
     });
   });
