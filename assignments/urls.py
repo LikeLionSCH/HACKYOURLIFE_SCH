290e8a9b49743989ef1910dcdf058e3c1cf0dcde
KimTaewan <wwan13@naver.com>
2020-12-23 15:06:05 +0900

    [taewan] : url 경로 수정

--- assignments/urls.py
+++ assignments/urls.py
@@ -3,6 +3,6 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path('assignment_create/',views.create_Assignment_view, name='assignment_create'),
-    path('assignment_list/',views.read_Assignment_view, name='assignment_list'),
+    path('create/',views.create_Assignment_view, name='assignment_create'),
+    path('list/',views.read_Assignment_view, name='assignment_list'),
 ]
