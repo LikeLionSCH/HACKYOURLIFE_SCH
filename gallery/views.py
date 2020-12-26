e81d09c07f6fc03f7f85b95a90c07bbba9aa3eae
김율희 <dbfgml741@likelion.org>
2020-12-27 05:07:21 +0900

    [yulhee] gallery read

--- gallery/views.py
+++ gallery/views.py
@@ -13,9 +13,6 @@ def gallery_main(request):
 def gallery_board(request):
     return render(request, "th_gallery_board.html")
 
-def gallery_detail(request):
-    return render(request, "gallery_board_detail.html")
-
 
 def gallery_create(request):
     if request.method == 'POST':
@@ -40,3 +37,21 @@ def gallery_create(request):
     # POST가 아닐 경우 assignment create 페이지 띄움
     return render(request, 'gallery_create.html')
 
+
+def gallery_detail(request):
+    # firebase initialize
+    db = initialize_firebase()
+
+    # template 으로 전달해줄 리스트 생성
+    galleries = []
+
+    # firebase 에 접근해 과제 목록들 불러옴
+    gallery_datas = db.collection('Gallery').stream()
+
+    # 값을 읽어와 하나씩 assignment_list 에 담는다
+    for gallery_data in gallery_datas:
+        gallery = Gallery.from_dict(gallery_data.to_dict(),gallery_data.id)
+        galleries.append(gallery)
+
+    # assignment_list 페이지 띄우고 과제 데이터 전달
+    return render(request,'gallery_board_detail.html',{'galleries':galleries})
