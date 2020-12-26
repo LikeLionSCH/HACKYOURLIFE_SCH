bd09891908187c620d01f70e7f7b923c3cf5fb73
KimTaewan <wwan13@naver.com>
2020-12-27 00:12:22 +0900

    [taewan] : url 연결

--- reports/urls.py
+++ reports/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('create/<str:assignment_id>/',views.create_Report_view,name='report_create'),
     path('list/<str:assignment_id>/',views.read_Report_list_view,name='report_list'),
-    path('detail/<str:report_id>/',views.get_Report_detail_view,name='report_detail'),
-    path('update/<str:report_id>/',views.update_Report_view,name='report_update'),
+    path('detail/<str:assignment_id>/<str:report_id>/',views.get_Report_detail_view,name='report_detail'),
+    path('update/<str:assignment_id>/<str:report_id>/',views.update_Report_view,name='report_update'),
     path('delete/<str:report_id>/',views.delete_Report,name='report_delete')
 ]
--- reports/views.py
+++ reports/views.py
@@ -8,6 +8,7 @@ from datetime import datetime
 
 from hackyourlife_sch.firebase import initialize_firebase
 from .models import Report
+from assignments.models import Assignment
 
 """
 레포트를 등록 해주는 함수
@@ -16,11 +17,20 @@ from .models import Report
 """
 def create_Report_view(request,assignment_id):
 
+    # 파이어베이스 초기화
+    db = initialize_firebase()
+
+    # 파이어베이스에서 매개변수로 불러온 과제의 데이터 불러옴
+    try:
+        assignment_data = db.collection('Assignment').document(assignment_id).get()
+    except google.cloud.exeption.NotFound:
+        print('report not found')
+
+    # 해당 과제의 제목 추출
+    assignment_title = assignment_data.to_dict()['title']
+
     # 리퀘스트의 메소드가 POST일 경우에만
     if (request.method=='POST'):
-        
-        # 파이어베이스 초기화
-        db = initialize_firebase()
 
         # form 으로부터 값을 불러오는 코드
         repository_address = request.POST['repository_address']
@@ -37,77 +47,115 @@ def create_Report_view(request,assignment_id):
         db.collection('Report').document().set(report.to_dict())
 
         # 리다이렉트
-        return redirect('/')
+        # 유저 타입 나뉘 경우 수정 해야함
+        return redirect('report_list',assignment_id)
+
+    output_datas = {
+        'assignment_id':assignment_id,
+        'assignment_title':assignment_title,
+    }
 
     # 포스트가 아닐경우 report_create 페이지 렌더
-    return render(request,'report_create.html', {'assignment_id':assignment_id})
+    return render(request,'report_create.html', output_datas)
 
 
 """
 레포트의 목록을 과제별로 나눠서 띄워주는 함수
 @param : request, 해당하는 과제의 id
-@return : report_list.html 렌더링, 해당하는 과제 목록들 전달
+@return : report_list.html 렌더링, output datas
 """
 def read_Report_list_view(request, assignment_id): 
     
     # 파이어 베이스 초기화
     db = initialize_firebase()
 
+    # 파이어베이스에서 매개변수로 불러온 과제의 데이터 불러옴
+    try:
+        assignment_data = db.collection('Assignment').document(assignment_id).get()
+    except google.cloud.exeption.NotFound:
+        print('report not found')
+
+    # 해당 과제의 제목 추출
+    assignment_title = assignment_data.to_dict()['title']
+
     # 파이어베이스에서 assignment_id 값이 매개변수로 불로온 과제의 id값과 일치하는 것만 가져오는 쿼리문
-    datas = db.collection('Report').where('assignment_id','==',assignment_id).stream()
+    report_datas = db.collection('Report').where('assignment_id','==',assignment_id).stream()
 
     reports = []
 
     # 템플릿으로 전달을 위해 불러온 데이터를 객체로 변환하여 전달할 리스트 생성
-    for data in datas:
-        report = Report.from_dict(data.to_dict(),data.id)
+    for report_data in report_datas:
+        report = Report.from_dict(report_data.to_dict(),report_data.id)
         reports.append(report)
 
-    # report_list.html 페이지 렌더링, 과제 목록 전달
-    return render(request,"report_list.html",{'reports':reports})
+    # 레포트 데이터들, 해당 과제의 Id, 해당 과제의 제목
+    output_datas = {
+        'reports':reports,
+        'assignment_id':assignment_data.id,
+        'assignment_title':assignment_title
+    }
+
+    # report_list.html 페이지 렌더링, output_datas
+    return render(request,"report_list.html",output_datas)
 
 
 
 """
 레포트의 디테일 뷰를 보여주는 함수
 @param : request, 보여줄 레포트의 id
-@return : report_detail.html 렌더링, 해당 레포트 데이터 전달
+@return : report_detail.html 렌더링, output datas
 """
-def get_Report_detail_view(request,report_id):
+def get_Report_detail_view(request,assignment_id,report_id):
     
     # 파이어베이스 초기화
     db = initialize_firebase()
 
-    # 매개변수의 레포트 id 값으로 파이어베이스에서 해당하는 데이터를 불러옴
+    # 매개변수의 id 값으로 파이어베이스에서 해당하는 데이터를 불러옴
     try:
-        data = db.collection('Report').document(report_id).get()
+        report_data = db.collection('Report').document(report_id).get()
+        assignment_data = db.collection('Assignment').document(assignment_id).get()
     except google.cloud.exeption.NotFound:
         print('report not found')
 
-    # 불러온 데이터로 레포트 객체 생성
-    report = Report.from_dict(data.to_dict(),data.id)
+    # 해당 과제의 제목 추출
+    assignment_title = assignment_data.to_dict()['title']
 
-    # 레포트 데테일 페이지 렌더링, 레포트 데이터 전달
-    return render(request,'report_detail.html',{'report':report})
+    # 불러온 데이터로 레포트 객체 생성
+    report = Report.from_dict(report_data.to_dict(),report_data.id)
+
+    # 레포트 목록, 해당하는 과제의 제목
+    output_datas = {
+        'report' : report,
+        'assignment_title' : assignment_title,
+        'assignment_id' : assignment_data.id,
+    }
+
+    # 레포트 데테일 페이지 렌더링, output datas
+    return render(request,'report_detail.html',output_datas)
+
 
 """
 제출한 과제를 수정하는 함수
 @param : request, 수정할 레포트의 id
 @return : report_update.html 렌더링, 수정한 레포트의 디테일 페이지로 리다이렉트
 """
-def update_Report_view(request,report_id):
+def update_Report_view(request,assignment_id,report_id):
     
     # 파이어베이스 초기화
     db = initialize_firebase()
 
-    # 매개변수의 레포트 id 값으로 파이어베이스에서 해당하는 데이터를 불러옴
+    # 매개변수의 id 값으로 파이어베이스에서 해당하는 데이터를 불러옴
     try:
-        data = db.collection('Report').document(report_id).get()
+        report_data = db.collection('Report').document(report_id).get()
+        assignment_data = db.collection('Assignment').document(assignment_id).get()
     except google.cloud.exeption.NotFound:
         print('report not found')
 
     # 파이어베이스에서 불러온 데이터로 객체 생성
-    report = Report.from_dict(data.to_dict(),data.id)
+    report = Report.from_dict(report_data.to_dict(),report_data.id)
+
+    # 과제의 제목 추출
+    assignment_title = assignment_data.to_dict()['title']
 
     if (request.method == 'POST'):
 
@@ -128,10 +176,17 @@ def update_Report_view(request,report_id):
         })
 
         # 수정 한 레포트의 디테일 페이지로 리다이렉트
-        return redirect('report_detail',report_id)
+        return redirect('report_detail',assignment_id,report_id)
+
+    # 레포트 데이터, 과제의 제목
+    output_datas = {
+        'report':report,
+        'assignment_title':assignment_title,
+        'assignment_id':assignment_data.id,
+    }
     
-    # report_update 렌더링, 레포트의 데이터 전달
-    return render(request,'report_update.html',{'report',report})
+    # report_update 렌더링, output datas
+    return render(request,'report_update.html',output_datas)
 
 
 """
@@ -139,7 +194,7 @@ def update_Report_view(request,report_id):
 @param : request, 삭제할 레포트의 id
 @return : 리스트 페이지로 리다이렉트
 """
-def delete_Report(request,report_id):
+def delete_Report(request,assignment_id,report_id):
      
     #파이어 베이스 초기화
     db = initialize_firebase()
