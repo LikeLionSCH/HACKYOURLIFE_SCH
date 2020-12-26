9e51e36892a21683fed2a1e89248afbdba700bdd
김율희 <dbfgml741@likelion.org>
2020-12-27 01:13:14 +0900

    [yulhee] gallery

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('gallery/', views.gallery_main, name="gallery_main"),
     path('gallery/board', views.gallery_board, name="gallery_board"),
-    path('faq/', views.gallery_detail, name="gallery_detail"),
+    path('gallery/board/detail', views.gallery_detail, name="gallery_detail"),
+    path('gallery/board/create', views.gallery_create, name="gallery_create"),
 ]
