ba0383cef52e75f5b49225c1aecd6c5fc4d88231
ningpop <craft1933@gmail.com>
2020-12-28 02:38:27 +0900

    [ningpop] : add session host submit function

--- static/js/session.js
+++ static/js/session.js
@@ -1,9 +1,12 @@
-var user = firebase.auth().currentUser;
+let user = firebase.auth().currentUser;
 
-if (user) {
-  // User is signed in.
-  document.getElementsById("host").value = user.displayName;
-} else {
-  // No user is signed in.
-  document.getElementsById("host").value = "Anonymous Host";
+try {
+  firebase.auth().onAuthStateChanged(function (user) {
+    // User is signed in.
+    document.getElementById("host").value = user.displayName;
+    console.log(user.displayName); // test code
+  })
+} catch (error) {
+  document.getElementById("host").value = "Anonymous Host";
+  console.log('현재 user값은 ', user, '입니다.'); // test code
 }
