60bdfb1978d037444624edac59b2818094790910
KimTaewan <wwan13@naver.com>
2020-12-29 01:24:50 +0900

    [taewan] : 레포트 페이지 세세한 구현 완료

--- reports/urls.py
+++ reports/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('list/<str:assignment_id>/',views.read_Report_list_view,name='report_list'),
     path('detail/<str:assignment_id>/<str:report_id>/',views.get_Report_detail_view,name='report_detail'),
     path('update/<str:assignment_id>/<str:report_id>/',views.update_Report_view,name='report_update'),
+    path('scoring/<str:assignment_id>/<str:report_id>/',views.scoring_Report_view,name='report_scoring'),
     path('delete/<str:assignment_id>/<str:report_id>/',views.delete_Report,name='report_delete')
 ]
