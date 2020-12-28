1d4c21eb177751711af921f29303384c3f8ab451
HwangSB <hsb990105@gmail.com>
2020-12-28 18:47:39 +0900

    [HwangSB] : Firestore에 등록된 유저만 Auth에 로그인 가능하도록 수정

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
 import firebase_admin
 from firebase_admin import credentials, auth
 from firebase_admin import firestore
@@ -21,6 +22,14 @@ def index(request, db):
     #         return render(request, 'main.html', data)
     #     else:
     #         return redirect('main')
+    if request.method == 'POST':
+        # 등록된 유저인지 검증
+        if request.POST['requestCode'] == 'verify_sign_in_user_request':
+            # 로그인을 시도하는 유저의 이메일과 일치하는 document가 하나 이상이면 로그인 승인
+            if db.collection('User').where('email', '==', request.POST['email']).get():
+                response = {'message': 'Current user is verified member.'}
+                return JsonResponse(response)
+            return HttpResponse('Current user is not verified member.', status=403)
 
     return render(request, 'main.html')
 
--- static/js/base.js
+++ static/js/base.js
@@ -13,33 +13,77 @@ function getCookie(name) {
     return cookieValue;
 }
 
-async function googleSignIn() {
-    // 이미 로그인 되어있으면, 로그아웃
-    let user = firebase.auth().currentUser;
-    if (user) {
-        return firebase.auth().signOut();
-    }
+// async function googleSignIn() {
+//     // 이미 로그인 되어있으면, 로그아웃
+//     let user = firebase.auth().currentUser;
+//     if (user) {
+//         return firebase.auth().signOut();
+//     }
 
-    // 창 닫기 전까지 로그인 상태 유지
-    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION).then(function() {
-        let provider = new firebase.auth.GoogleAuthProvider();
+//     // 창 닫기 전까지 로그인 상태 유지
+//     firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION).then(function() {
+//         let provider = new firebase.auth.GoogleAuthProvider();
         
-        // 팝업창으로 구글 로그인
-        return firebase.auth().signInWithPopup(provider).then(function (result) {
-            // 로그인한 유저의 uid를 서버에 전송
-            $.ajax({
-                type: "POST",
-                url: "/",
-                headers: {
-                    "X-CSRFToken": getCookie('csrftoken')
-                },
-                dataType: "json",
-                data: {
-                    uid: result.user.uid
-                }
-            });
-        });
+//         // 팝업창으로 구글 로그인
+//         return firebase.auth().signInWithPopup(provider).then(function (result) {
+//             // 로그인한 유저의 uid를 서버에 전송
+//             $.ajax({
+//                 type: "POST",
+//                 url: "/",
+//                 headers: {
+//                     "X-CSRFToken": getCookie('csrftoken')
+//                 },
+//                 dataType: "json",
+//                 data: {
+//                     uid: result.user.uid
+//                 }
+//             });
+//         });
+//     });
+// }
+
+function verifyUser(email, onSuccess, onFail) {
+    $.ajax({
+        type: "POST",
+        url: "/",
+        headers: {
+            "X-CSRFToken": getCookie('csrftoken')
+        },
+        dataType: "json",
+        data: {
+            requestCode: 'verify_sign_in_user_request',
+            email: email
+        },
+        success: onSuccess,
+        error: onFail
     });
+}
+
+// 테스트할 때 반드시 localhost:8000으로 접속하세요
+function onSignIn(googleUser) {
+    let email = googleUser.getBasicProfile().getEmail();
+    verifyUser(email,
+        // 등록된 유저인 경우
+        function() {
+            let idToken = googleUser.getAuthResponse().id_token;
+            let credential = firebase.auth.GoogleAuthProvider.credential(idToken);
+            firebase.auth().signInWithCredential(credential).then(function() {
+                console.log("signed in");
+            });
+        },
+        // 등록되지 않은 유저인 경우
+        function() {
+            console.log(response);
+
+            let result = confirm("등록되지 않은 사용자 입니다. 승인 신청?");
+            if (result) {
+                // 승인 신청
+                console.log(`${email}, 7기/8기/9기, 멤버/운영진`);
+            }
+
+            gapi.auth2.getAuthInstance().signOut();
+        }
+    );
 }
 
 // Firebase 초기화
