46692dc88756a1376d0596787fa8cbd8beff468b
HwangSB <hsb990105@gmail.com>
2020-12-30 16:12:06 +0900

    [HwangSB] : 캘린더가 2020년 1월 이후로 모든 이벤를 표시하도록 변경 FIXME: 월 전환시마다 캘린더 다시 불러오도록 수정해야함

--- google_calendar/views.py
+++ google_calendar/views.py
@@ -25,12 +25,19 @@ def calendar(request):
         now = datetime.utcnow().isoformat() + 'Z'
         last_day = monthrange(datetime.utcnow().year, datetime.utcnow().month)[1]
 
+        # TODO: full calendar api 사용해서 월 전환될 때마다 캘린더 다시 불러오기
+        # 시간이 없다 ㅠ 9기들아 부탁해!!!
+
         start_of_month = datetime.utcnow().replace(day=1, hour=0, minute=0, second=0, microsecond=0).isoformat() + 'Z'
         end_of_month = datetime.utcnow().replace(day=last_day, hour=23, minute=59, second=59, microsecond=999).isoformat() + 'Z'
 
+        # FIXME: 일단 8기 활동을 전부 표시하는 방식으로 임시방편..
+        start_of_month = datetime(2020, 1, 1).isoformat() + 'Z'
+        end_of_month = None
+
         print(start_of_month, end_of_month)
 
-        events_result = service.events().list(calendarId=CALENDAR_ID, timeMin=start_of_month, timeMax=None, singleEvents=True, orderBy='startTime').execute()
+        events_result = service.events().list(calendarId=CALENDAR_ID, timeMin=start_of_month, timeMax=end_of_month, singleEvents=True, orderBy='startTime').execute()
         events = events_result.get('items', [])
 
         data = []
