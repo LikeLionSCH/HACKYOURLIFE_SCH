73f4dc2c6570619dc4ff72cbe3ddc0f84b2b1ff1
HwangSB <hsb990105@gmail.com>
2021-01-03 04:57:53 +0900

    [HwangSB] : GAE 배포 완료

--- hackyourlife_sch/settings.py
+++ hackyourlife_sch/settings.py
@@ -138,5 +138,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = Path(BASE_DIR, '.static')
+STATIC_URL = 'https://storage.googleapis.com/hackyourlife/static/'
+STATIC_ROOT = Path(BASE_DIR, 'static')
